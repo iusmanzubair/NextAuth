acf8f78655416032f89afe8db73cb43afad7ecc3
Usman <usmanzubair091@gmail.com>
2024-02-27 20:54:28 +0500

    card-wrapper.tsx added

---
+++ components/auth/LoginButton.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+interface loginButtonProps {
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+}
+
+const LoginButton = ({
+  children,
+  mode = "redirect",
+  asChild,
+}: loginButtonProps) => {
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push("/auth/login");
+  };
+  return (
+    <span onClick={onClick} className="cursor-pointer">
+      {children}
+    </span>
+  );
+};
+
+export default LoginButton;
